c47cf5c7ef22ea2333f9b1252137208fabd48833
omar-labana <olabana@effvision.com>
2025-05-13 21:43:31 +0300

    feat: remove main.js; update main.ts for consistent logging format; add .prettierrc.yml for code formatting

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
-import { consola, createConsola } from "consola";
+import { consola, createConsola } from 'consola';
 
-consola.info("Using consola 3.0.0");
-consola.start("Building project...");
-consola.warn("A new version of consola is available: 3.0.1");
-consola.success("Project built!");
-consola.error(new Error("This is an example error. Everything is fine!"));
-consola.box("I am a simple box");
-await consola.prompt("Deploy to the production?", {
-  type: "confirm",
+consola.info('Using consola 3.0.0');
+consola.start('Building project...');
+consola.warn('A new version of consola is available: 3.0.1');
+consola.success('Project built!');
+consola.error(new Error('This is an example error. Everything is fine!'));
+consola.box('I am a simple box');
+await consola.prompt('Deploy to the production?', {
+  type: 'confirm',
 });
